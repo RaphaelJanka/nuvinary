a87d40ae1e6d45991f863a7b08b9b6051113916e
Raphael Janka <raphael.janka@gmail.com>
2026-02-26 16:22:41 +0100

    declaring first routes and components

--- nuvinary-frontend/src/app/app.ts
+++ nuvinary-frontend/src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('nuvinary-frontend');
+export class App implements OnInit {
+  private readonly titleService = inject(Title);
+  protected readonly title = signal('Nuvinary');
+
+  ngOnInit(): void {
+    this.titleService.setTitle(this.title());
+  }
 }
