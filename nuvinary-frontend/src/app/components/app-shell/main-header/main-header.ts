51bcfb09989b103950c13e63f2a7d3feae1a2919
Raphael Janka <raphael.janka@gmail.com>
2026-03-01 19:17:45 +0100

    Installed icons + updated colors

--- nuvinary-frontend/src/app/components/app-shell/main-header/main-header.ts
+++ nuvinary-frontend/src/app/components/app-shell/main-header/main-header.ts
@@ -1,13 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { SidebarService } from '../services/sidebar-service';
+import { LucideAngularModule, Menu } from 'lucide-angular';
 
 @Component({
   selector: 'app-main-header',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './main-header.html',
 })
 export class MainHeader {
   private sidebarService = inject(SidebarService);
+  readonly menuIcon = Menu;
 
   isCollapsed = this.sidebarService.isCollapsed;
 
