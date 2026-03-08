075dbefa23d45282d5a77af241c665610307d461
Raphael Janka <raphael.janka@gmail.com>
2026-03-08 09:04:00 +0100

    adjusted dashboard component, added sampl images for gallery styling

--- nuvinary-frontend/src/app/features/dashboard/pages/gallery/gallery.ts
+++ nuvinary-frontend/src/app/features/dashboard/pages/gallery/gallery.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Folder, LucideAngularModule, Plus } from 'lucide-angular';
+import { Component, signal } from '@angular/core';
+import { Folder, LucideAngularModule, Maximize, Plus, Search } from 'lucide-angular';
 
 @Component({
   selector: 'app-gallery',
@@ -9,4 +9,15 @@ import { Folder, LucideAngularModule, Plus } from 'lucide-angular';
 export class Gallery {
   protected readonly plusIcon = Plus;
   protected readonly folderIcon = Folder;
+  protected readonly searchIcon = Search;
+  protected readonly maximizeIcon = Maximize;
+  selectedImageId = signal<number | null>(null);
+
+  protected onOpenDetails(id: number) {
+    this.selectedImageId.set(id);
+  }
+
+  closeDetails() {
+    this.selectedImageId.set(null);
+  }
 }
