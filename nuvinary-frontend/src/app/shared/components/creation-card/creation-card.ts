1c4614c42f4995e580db2ccb4ab66fad207a8184
Raphael Janka <raphael.janka@gmail.com>
2026-03-17 15:23:08 +0100

    improvements for opening cards

--- nuvinary-frontend/src/app/shared/components/creation-card/creation-card.ts
+++ nuvinary-frontend/src/app/shared/components/creation-card/creation-card.ts
@@ -1,16 +1,18 @@
 import { Component, input, output } from '@angular/core';
 import { Creation } from '../../models/creation.model';
-import { LucideAngularModule, Maximize } from 'lucide-angular';
 
 @Component({
   selector: 'app-creation-card',
-  imports: [LucideAngularModule],
+  imports: [],
   templateUrl: './creation-card.html',
 })
 export class CreationCard {
   readonly creation = input.required<Creation>();
-
   openDetails = output<Creation>();
 
-  protected readonly maximizeIcon = Maximize;
+  onOpenDetails() {
+    if (this.creation().status === 'completed') {
+      this.openDetails.emit(this.creation());
+    }
+  }
 }
