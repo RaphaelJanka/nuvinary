498ec7fac01440329fd5197ef10ae89e3970a2a9
Raphael Janka <raphael.janka@gmail.com>
2026-05-18 16:40:12 +0200

    removed vanta

--- nuvinary-frontend/src/app/features/hero/hero.ts
+++ nuvinary-frontend/src/app/features/hero/hero.ts
@@ -1,21 +1,5 @@
-import {
-  afterNextRender,
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  HostListener,
-  inject,
-  NgZone,
-  OnDestroy,
-  viewChild,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Header } from '../../shared/components/header/header';
-import * as THREE from 'three';
-import CLOUDS, { VantaEffect } from 'vanta/dist/vanta.clouds.min.js';
-
-interface VantaWindow extends Window {
-  THREE?: typeof THREE;
-}
 
 @Component({
   selector: 'app-hero',
@@ -23,45 +7,4 @@ interface VantaWindow extends Window {
   templateUrl: './hero.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Hero implements OnDestroy {
-  private vantaEffect?: VantaEffect;
-  private readonly vantaContainer = viewChild.required<ElementRef<HTMLElement>>('vantaContainer');
-  private readonly ngZone = inject(NgZone);
-
-  @HostListener('window:resize')
-  onResize() {
-    this.ngZone.runOutsideAngular(() => {
-      this.vantaEffect?.resize();
-    });
-  }
-
-  constructor() {
-    afterNextRender(() => {
-      this.ngZone.runOutsideAngular(() => {
-        (window as VantaWindow).THREE = THREE;
-
-        this.vantaEffect = CLOUDS({
-          el: this.vantaContainer().nativeElement,
-          THREE: THREE,
-          mouseControls: false,
-          touchControls: false,
-          minHeight: 200.0,
-          minWidth: 200.0,
-          speed: 0.3,
-          skyColor: 0xedadb0,
-          cloudColor: 0xadadde,
-          cloudShadowColor: 0x183550,
-          sunColor: 0xffbb18,
-          sunGlareColor: 0xffb530,
-          sunLightColor: 0xff9130,
-        });
-      });
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.vantaEffect) {
-      this.vantaEffect.destroy();
-    }
-  }
-}
+export class Hero {}
